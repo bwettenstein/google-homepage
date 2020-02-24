461fe4141f0707c7a429eccead277a11e1490be4
bwettenstein <wettenstein@gmail.com>
2020-02-23 23:38:46 -0800

    footer dark mode

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let headerElement = document.getElementsByClassName('header-element');
+const headerElements = document.getElementsByClassName('header-element');
+const footerElements = document.getElementsByClassName('footer-element');
+const footer = document.querySelector('footer');
+
 // let gridIcon = document.getElementsByTagName('i');
 
 document.querySelector('.theme-changer').addEventListener('click', () => {
     document.body.classList.toggle('dark');
-    for(let x = 0; x<4; x++) {
-        headerElement[x].classList.toggle('dark');
+    for(let x = 0; x<headerElements.length; x++) {
+        headerElements[x].classList.toggle('dark');
     }
+    for(let y = 0; y < footerElements.length; y++) {
+        footerElements[y].classList.toggle('dark');
+    }
+    footer.classList.toggle('dark');
 })
