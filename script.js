95291b85d65a97210404afa2e34ab40413f03289
bwettenstein <wettenstein@gmail.com>
2020-02-24 21:25:26 -0800

    Added dark mode for all elements

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const headerElements = document.getElementsByClassName('header-element');
 const footerElements = document.getElementsByClassName('footer-element');
+const buttonElements = document.getElementsByClassName('button-element');
 const footer = document.querySelector('footer');
+const searchBar = document.querySelector('.search-bar');
 
 // let gridIcon = document.getElementsByTagName('i');
 
-document.querySelector('.theme-changer').addEventListener('click', () => {
+document.querySelector('#theme-changer').addEventListener('click', () => {
     document.body.classList.toggle('dark');
     for(let x = 0; x<headerElements.length; x++) {
         headerElements[x].classList.toggle('dark');
@@ -12,5 +14,9 @@ document.querySelector('.theme-changer').addEventListener('click', () => {
     for(let y = 0; y < footerElements.length; y++) {
         footerElements[y].classList.toggle('dark');
     }
+    for(let z = 0; z < buttonElements.length; z++) {
+        buttonElements[z].classList.toggle('dark');
+    }
     footer.classList.toggle('dark');
+    searchBar.classList.toggle('dark');
 })
